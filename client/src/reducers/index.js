48e1b04f8d6d39b6dec052bb81e14bacfec3c48b
Deoshii <163427616+zero-xdev@users.noreply.github.com>
2025-04-25 23:36:00 +0800

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,12 @@
+// Importing the 'combineReducers' function from Redux to combine individual reducers
 import { combineReducers } from 'redux';
 
-import posts from './posts';
+// Importing the 'posts' reducer, which handles actions related to posts in the application
+import posts from './posts';  // The posts reducer is imported from the relative path
 
-export const reducers = combineReducers({ posts });
+// The 'combineReducers' function takes an object where each key corresponds to a slice of the state
+// Here, we are combining the 'posts' reducer with the overall application state
+// The resulting 'reducers' object can be used to configure the Redux store
+export const reducers = combineReducers({
+  posts  // Associating the 'posts' reducer with the 'posts' key in the state
+});
