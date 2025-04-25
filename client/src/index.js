93be9a606c49ba7b53e3d42a472d4ad6619ff06b
Deoshii <163427616+zero-xdev@users.noreply.github.com>
2025-04-25 23:23:18 +0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import { reducers } from './reducers';
-import App from './App';
-import './index.css';
+import { reducers } from './reducers';  // Importing the combined reducers
+import App from './App';  // The main application component
+import './index.css';  // Importing the global CSS file for styling
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+// Creating the Redux store with middleware (thunk) for asynchronous actions
+const store = createStore(
+  reducers,  // The root reducer, combining all individual reducers
+  compose(  // Composing the store with applied middleware
+    applyMiddleware(thunk)  // Adding redux-thunk to handle async actions
+  )
+);
 
+// Rendering the React app and wrapping it with the Redux Provider to connect the store
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={store}>  {/* Provider component passes the store to the entire app */}
+    <App />  {/* The root component of the application */}
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')  // Attaching the app to the DOM element with the id 'root'
 );
